7f126f1174a6d86880de76b241ad655867715c46
Thiago Lima <thiagolima.unb@gmail.com>
2022-02-07 10:45:29 -0300

    feat: add exercism fsharp cars assemble

---
+++ exercism/fsharp/cars-assemble/CarsAssemble.fs
@@ -0,0 +1,15 @@
+module CarsAssemble
+
+let successRate (speed: int): float =
+    if speed = 0 then 0
+    elif speed < 5 then 1
+    elif speed < 9 then 0.9
+    elif speed < 10 then 0.8
+    else 0.77
+
+let productionRatePerHour (speed: int): float =
+    let carsPerHour = 221.0
+    carsPerHour * float speed * successRate speed
+
+let workingItemsPerMinute (speed: int): int =
+    int (productionRatePerHour speed / 60.0)
