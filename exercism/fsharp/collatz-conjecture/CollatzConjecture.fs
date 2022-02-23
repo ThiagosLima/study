1fa723f3f08c2ef08002645c5ebdcf6f4a6cadc0
Thiago Lima <thiagolima.unb@gmail.com>
2022-02-22 23:11:21 -0300

    feat: add exercism fsharp collatz conjecture

---
+++ exercism/fsharp/collatz-conjecture/CollatzConjecture.fs
@@ -0,0 +1,16 @@
+﻿module CollatzConjecture
+
+let (|Even|Odd|Invalid|FinalStep|) number =
+    match number with
+    | 1 -> FinalStep
+    | n when n <= 0 -> Invalid
+    | n when n % 2 = 0 -> Even
+    | _ -> Odd
+
+let rec steps (number: int): int option =
+    let addStep = Option.map ((+) 1)
+    match number with
+    | Invalid -> None
+    | FinalStep -> Some 0
+    | Even -> addStep (steps (number / 2))
+    | Odd -> addStep (steps (3 * number + 1))
