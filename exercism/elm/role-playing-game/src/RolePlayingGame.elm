25c26fe34c3de9debf41e1ef14262c70e27cb96d
Thiago Lima <thiagolima.unb@gmail.com>
2022-04-08 09:29:21 -0300

    feat: add exercism elm role-playing-game

---
+++ exercism/elm/role-playing-game/src/RolePlayingGame.elm
@@ -0,0 +1,41 @@
+module RolePlayingGame exposing (Player, castSpell, introduce, revive)
+
+
+type alias Player =
+    { name : Maybe String
+    , level : Int
+    , health : Int
+    , mana : Maybe Int
+    }
+
+
+introduce : Player -> String
+introduce { name } =
+    Maybe.withDefault "Mighty Magician" name
+
+
+revive : Player -> Maybe Player
+revive player =
+    case ( player.health, player.mana ) of
+        ( 0, Just _ ) ->
+            Just { player | health = 100, mana = Just 100 }
+
+        ( 0, Nothing ) ->
+            Just { player | health = 100 }
+
+        _ ->
+            Nothing
+
+
+castSpell : Int -> Player -> ( Player, Int )
+castSpell manaCost player =
+    case player.mana of
+        Just mana ->
+            if manaCost > mana then
+                ( player, 0 )
+
+            else
+                ( { player | mana = Just (mana - manaCost) }, manaCost * 2 )
+
+        Nothing ->
+            ( { player | health = max (player.health - manaCost) 0 }, 0 )
